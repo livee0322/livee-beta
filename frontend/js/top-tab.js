051ab986d79936c734a48b25d11c1689853bbf58
livee0322 <dlxodnd6564@naver.com>
2025-08-01 02:02:37 +0900

    top-tab.js 업데이트

--- frontend/js/top-tab.js
+++ frontend/js/top-tab.js
@@ -1,11 +1,7 @@
 document.getElementById('top-tab-container').innerHTML = `
-  <nav class="top-tabs">
-    <a href="./index.html" class="tab">홈</a>
-    <a href="./clip.html" class="tab">숏클립</a>
-    <a href="./event.html" class="tab">이벤트</a>
-    <a href="./live.html" class="tab">라이브</a>
-    <a href="./influencer.html" class="tab">인플루언서</a>
-    <a href="./agency.html" class="tab">대행서비스</a>
-    <a href="./news.html" class="tab">뉴스</a>
+  <nav class="top-tab">
+    <a href="/livee-beta/index.html">홈</a>
+    <a href="/livee-beta/frontend/showhost.html">쇼호스트</a>
+    <a href="/livee-beta/frontend/login.html">로그인</a>
   </nav>
 `;
