55bd8fcfbdcd3d357d523231889a1b2ef7bb886b
livee0322 <dlxodnd6564@naver.com>
2025-08-01 03:00:38 +0900

    bottom-tab.js 업데이트

--- frontend/js/bottom-tab.js
+++ frontend/js/bottom-tab.js
@@ -1,6 +1,6 @@
 document.getElementById('bottom-tab-container').innerHTML = `
   <nav class="bottom-tab">
-    <a href="/livee-beta/index.html" class="${location.pathname.endsWith('index.html') ? 'active' : ''}">홈</a>
+    <a href="/livee-beta/index.html" class="${location.pathname.includes('index') || location.pathname === '/livee-beta/' ? 'active' : ''}">홈</a>
     <a href="/livee-beta/frontend/showhost.html" class="${location.pathname.includes('showhost') ? 'active' : ''}">쇼호스트</a>
     <a href="/livee-beta/frontend/login.html" class="${location.pathname.includes('login') ? 'active' : ''}">로그인</a>
   </nav>
