caa34df07467d42101fb6a3c40b8f04d93413275
livee0322 <dlxodnd6564@naver.com>
2025-08-01 02:02:17 +0900

    bottom-tab.js 업데이트

--- frontend/js/bottom-tab.js
+++ frontend/js/bottom-tab.js
@@ -1,12 +1,7 @@
-const token = localStorage.getItem('liveeToken');
-const myLink = token ? './mypage.html' : './login.html';
-
 document.getElementById('bottom-tab-container').innerHTML = `
   <nav class="bottom-tab">
-    <a href="./index.html" class="tab">홈</a>
-    <a href="./live.html" class="tab">라이브</a>
-    <a href="./showhost.html" class="tab">쇼호스트</a>
-    <a href="./schedule.html" class="tab">일정</a>
-    <a href="${myLink}" class="tab">마이</a>
+    <a href="/livee-beta/index.html" class="${location.pathname.endsWith('index.html') ? 'active' : ''}">홈</a>
+    <a href="/livee-beta/frontend/showhost.html" class="${location.pathname.includes('showhost') ? 'active' : ''}">쇼호스트</a>
+    <a href="/livee-beta/frontend/login.html" class="${location.pathname.includes('login') ? 'active' : ''}">로그인</a>
   </nav>
 `;
